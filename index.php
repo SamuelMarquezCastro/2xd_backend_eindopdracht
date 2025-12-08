790d6a5fe1940301767a6d35f79700cf0fd14be9
Samuel Marquez Castro <r1042674@student.thomasmore.be>
2025-12-08 15:33:12 +0100

    feat: navigatie, hero sectie, img folder

--- index.php
+++ index.php
@@ -3,10 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zara Home</title>
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+
+
 </head>
 <body>
+
+    <header class="header">
+        <div class="logo">
+            <img src="img/Zara_Home_logo_2023.png" alt="Zara Home logo">
+        </div>
+
+        <nav class="nav">
+            <ul>
+                <li><a href="#">Search</a></li>
+                <li><a href="#">Collectie</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Winkelmand</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="hero">
+        <div class="hero-inner">
+            <h2>A World Of Wonder</h2>
+            <button>Bekijk de collectie</button>
+        </div>
+    </section>
+
+    <main>
     
+    </main>
+
 </body>
 </html>
